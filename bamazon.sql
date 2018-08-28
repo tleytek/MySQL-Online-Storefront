3dd3d10128b754103994a8eec26d3a49012cab36
tleytek <lucidmat94@gmail.com>
2018-08-28 15:41:20 -0400

    Prompt user with 2 questions using inquirer

--- bamazon.sql
+++ bamazon.sql
@@ -4,25 +4,30 @@ CREATE DATABASE bamazonDB
 
 USE bamazonDB;
 
-CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(30) NULL,
-    department_name VARCHAR(30) NULL,
-    price DECIMAL(10,2) NULL,
+CREATE TABLE products
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    product_name VARCHAR
+    (30) NULL,
+    department_name VARCHAR
+    (30) NULL,
+    price DECIMAL
+    (10,2) NULL,
     stock_quantity INT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
 
-INSERT INTO products (product_name, department_name, price, quantity)
-VALUES ("Pop up Tent", "Outdoor Gear", 45.89, 150);
-
-INSERT INTO products (product_name, department_name, price, quantity)
-VALUES ("Sling Shoulder Backpack", "Outdoor Gear", 20.37, 300), 
-("Sneakers", "Clothing", 45.00, 50),
-('Baseball Cap', 'Clothing', 20.00, 250),
-('Ab Wheel', "Exercise & Fitness", 14.28, 100),
-('Fitness Tracker (Fitbit)', "Exercise & Fitness", 44.99, 300),
-('Microsoft Sculpt Ergonomic Keyboard', "Office Products", 88.47, 120),
-('High-Back Executive Chair', "Office Products", 149.99, 50),
-('NERF Gun', 'Toys', 18.87, 250),
-('Jump Rope', 'Toys', 17.83, 175)
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Sling Shoulder Backpack", "Outdoor Gear", 20.37, 300),
+        ("Sneakers", "Clothing", 45.00, 50),
+        ('Baseball Cap', 'Clothing', 20.00, 250),
+        ('Ab Wheel', "Exercise & Fitness", 14.28, 100),
+        ('Fitness Tracker (Fitbit)', "Exercise & Fitness", 44.99, 300),
+        ('Microsoft Sculpt Ergonomic Keyboard', "Office Products", 88.47, 120),
+        ('High-Back Executive Chair', "Office Products", 149.99, 50),
+        ('NERF Gun', 'Toys', 18.87, 250),
+        ('Jump Rope', 'Toys', 17.83, 175)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
-  database: "bamazon"
+  database: "bamazondb"
 });
 
 connection.connect(function(err) {
@@ -17,7 +18,31 @@ connection.connect(function(err) {
 function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log(res);
+
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item ID " + res[i].id + ":");
+      console.log("Department: " + res[i].department_name);
+      console.log("Item Name: " + res[i].product_name);
+      console.log("Item Price: $" + res[i].price);
+      console.log("Stock: " + res[i].stock_quantity);
+      console.log("-----------------------------------");
+    }
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "itemID",
+          message: "Input the ID # of the item you would like to purchase."
+        },
+        {
+          type: "input",
+          name: "itemQuantity",
+          message: "How many would you like?"
+        }
+      ])
+      .then(function(answer) {});
+
     connection.end();
   });
 }
