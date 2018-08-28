79d55a627f04a26000a7ba63a941b7d3197fe72b
tleytek <lucidmat94@gmail.com>
2018-08-27 21:17:11 -0400

    node sql connection, mock data sql

--- bamazon.sql
+++ bamazon.sql
@@ -18,10 +18,11 @@ VALUES ("Pop up Tent", "Outdoor Gear", 45.89, 150);
 
 INSERT INTO products (product_name, department_name, price, quantity)
 VALUES ("Sling Shoulder Backpack", "Outdoor Gear", 20.37, 300), 
-
-("Sneakers", "Clothing", 45.00, 50)
-('Baseball Cap', 'Clothing', 20.00, 250)
-
-('Ab Wheel', "Exercise & Fitness", 14.28, 100)
-('Fitness Tracker (Fitbit)', "Exercise & Fitness", 44.99, 300)
-
+("Sneakers", "Clothing", 45.00, 50),
+('Baseball Cap', 'Clothing', 20.00, 250),
+('Ab Wheel', "Exercise & Fitness", 14.28, 100),
+('Fitness Tracker (Fitbit)', "Exercise & Fitness", 44.99, 300),
+('Microsoft Sculpt Ergonomic Keyboard', "Office Products", 88.47, 120),
+('High-Back Executive Chair', "Office Products", 149.99, 50),
+('NERF Gun', 'Toys', 18.87, 250),
+('Jump Rope', 'Toys', 17.83, 175)
